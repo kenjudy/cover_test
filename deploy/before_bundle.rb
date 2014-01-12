6f23cd5b6fef2a2e0523173ae77b5adfb19b33a8
Ken Judy <ken.judy@simonandschuster. com>
2014-01-12 10:36:14 -0500

    add deploy hook for private configs

---
+++ deploy/before_bundle.rb
@@ -0,0 +1,2 @@
+run "ln -s /data/cover_test/shared/config/s3.yml /data/cover_test/current/config/s3.yml" 
+run "ln -s /data/cover_test/shared/config/blitline.yml /data/cover_test/current/config/blitline.yml" 
