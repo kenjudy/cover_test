d3e4186d3750ecc35e2e8129787c0fb0a970ca9b
Ken Judy <ken.judy@simonandschuster. com>
2014-01-12 10:42:17 -0500

    adjust after restart hook to accomodate possibility of no sample images in project

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -1,2 +1,3 @@
 run "rm -rf /data/cover_test/current/public/images/covers"
+run "mkdir /data/cover_test/current/public/images"
 run "ln -s /data/cover_test/shared/covers /data/cover_test/current/public/images/covers" 
